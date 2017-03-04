1081f49e30cddd92e4284780e0351fcfd9c76c5c
Nicholas Monath <nicholas.monath@gmail.com>
2017-03-04 13:45:31 -0500

    fix script, add glass script

--- bin/download_data.sh
+++ bin/download_data.sh
@@ -2,5 +2,5 @@
 
 set -exu
 
-./$XCLUSTER_ROOT/bin/data_processing/download_aloi.sh
-./$XCLUSTER_ROOT/bin/data_processing/download_glass.sh
+$XCLUSTER_ROOT/bin/data_processing/download_aloi.sh
+$XCLUSTER_ROOT/bin/data_processing/download_glass.sh
