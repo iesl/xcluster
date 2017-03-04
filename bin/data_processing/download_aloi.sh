3b979cf08b435f80e928c592c7167c926ac0073a
Nicholas Monath <nicholas.monath@gmail.com>
2017-03-04 13:56:17 -0500

    fix script

--- bin/data_processing/download_aloi.sh
+++ bin/data_processing/download_aloi.sh
@@ -4,5 +4,5 @@
 pushd $XCLUSTER_DATA
 wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/multiclass/aloi.scale.bz2
 bzcat aloi.scale.bz2 > aloi.scale.raw
-python $XCLUSTER_ROOT/bin-clean/data_processing/process_aloi.py aloi.scale.raw aloi.tsv
+python $XCLUSTER_ROOT/bin/data_processing/process_aloi.py aloi.scale.raw aloi.tsv
 popd
