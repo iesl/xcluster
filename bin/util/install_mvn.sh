41b78e1a5cd3402739511004e025c1dde5304d98
Nicholas Monath <nmonath@users.noreply.github.com>
2019-08-20 18:10:16 -0400

    Update install_mvn.sh

--- bin/util/install_mvn.sh
+++ bin/util/install_mvn.sh
@@ -4,6 +4,6 @@ set -xu
 
 mkdir $XCLUSTER_ROOT/dep
 pushd $XCLUSTER_ROOT/dep
-wget http://mirror.cc.columbia.edu/pub/software/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
-tar -xvf apache-maven-3.3.9-bin.tar.gz
+wget http://mirror.cc.columbia.edu/pub/software/apache/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
+tar -xvf apache-maven-3.6.1-bin.tar.gz
 popd
